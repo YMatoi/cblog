8f8aa1edee5218bf65633cfda2c9f73ed2b61f62
YMatoi <YMatoi@users.noreply.github.com>
2018-01-03 04:42:03 +0900

    コメント機能を追加

--- src-cljs/main/article-view.cljs
+++ src-cljs/main/article-view.cljs
@@ -16,25 +16,69 @@
         (swap! html conj [:p line]))
       @html))) 
 
+(defn comments-view [id]
+  (let [input (reagent/atom {})
+        comments (reagent/atom [:div.comments])]
+    (go (let [response (<! (http/get (str "/v1/articles/" id "/comments")))
+              status (:status response)
+              body (get-in response [:body :response])]
+          (cond
+            (= 200 status) (doseq [c body]
+                             (println (:body c))
+                             (println (:user_id c))
+                             (swap! comments conj [ui/card
+                                                   [ui/card-header {:title (:body c)
+                                                                    :subtitle (:user_id c)}]])))))
+    (fn []
+      [:div
+       [:div.input
+        [ui/text-field {:floating-label-text "Comment"
+                        :on-change #(swap! input assoc :body %2)}]
+        [ui/raised-button {:label "Add Comment"
+                           :primary true
+                           :on-click #(go (let [response (<! (http/post (str "/v1/articles/" id "/comments")
+                                                                        {:json-params @input
+                                                                         :headers {"Authorization" (str "Token " (storage/get-token))}}))
+                                                status (:status response)
+                                                title (get-in response [:body :response 0 :body])
+                                                user-id (get-in response [:body :response 0 :user_id])]
+                                            (cond
+                                              (= 200 status)
+                                              (do (println response) (swap! comments conj [ui/card
+                                                                        [ui/card-header {:title title
+                                                                                         :subtitle user-id}]])))))}]]
+      @comments]))) 
+
+(defn article-card [body id app-routes]
+  [ui/card
+   [ui/card-header {:title (:title body)
+                    :subtitle (:user_id body)}]
+   [ui/card-text [article-body (:body body)]]
+   (if (and
+           (not (nil? (storage/get-user-id)))
+           (= (storage/get-user-id) (:user_id body)))
+     [ui/card-actions
+      [ui/raised-button {:label "Delete"
+                         :secondary true
+                         :on-click #(go (let [response (<! (http/delete (str "/v1/articles/" id)
+                                                                        {:headers {"Authorization" (str "Token " (storage/get-token))}}))]
+                                          (set! (.-location js/document) (bidi/path-for app-routes :articles))))}]
+      [ui/raised-button {:label "Edit"
+                         :primary true
+                         :href (bidi/path-for app-routes :article-update :id id)}]]
+     [:div])])
+
 (defn component [app-routes]
   (let [routing-data (session/get :route)
         item (get-in routing-data [:route-params :id])
         article (reagent/atom [:div.article])]
-    (fn []
-      (when (<= (count @article) 1)
-        (go (let [response (<! (http/get (str "/v1/articles/" item)))
-                  status (:status response)
-                  body (get-in response [:body :response])]
-              (cond
-                (= 200 status) (do (swap! article conj [ui/card 
-                                                        [ui/card-header {:title (:title body)
-                                                                         :subtitle (:user_id body)}]
-                                                        [ui/card-text [article-body (:body body)]]])
-                                   (when (= (storage/get-user-id) (:user_id body))
-                                     (swap! article conj [ui/flat-button {:label "Delete" :secondary true
-                                                                          :on-click #(go (let [response (<! (http/delete (str "/v1/articles/" item) {:headers {"Authorization" (str "Token " (storage/get-token))}}))]
-                                                                                           (set! (.-location js/document) (bidi/path-for app-routes :articles))))}])
-                                     (swap! article conj [ui/flat-button {:label "Edit"
-                                                                          :href (bidi/path-for app-routes :article-update :id item)}])))
-                :else (do (reset! article [:div "Unknown Error"]))))))
-      @article)))
+    (go (let [response (<! (http/get (str "/v1/articles/" item)))
+              status (:status response)
+              body (get-in response [:body :response])]
+          (cond
+            (= 200 status) (do (swap! article conj [article-card body item app-routes])) 
+            :else (do (reset! article [:div "Unknown Error"])))))
+    (fn [] 
+      [:div
+        @article
+        [comments-view item]])))
